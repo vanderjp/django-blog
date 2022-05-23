0d442ae81a96a41624c43c0d5e10a3a0a4c5a97e
Jared van der Duim <jared.vanderduim47@t-mobile.com>
2022-05-22 23:35:11 -0400

    Display details of individual post.

--- mysite/blogging/views.py
+++ mysite/blogging/views.py
@@ -22,5 +22,14 @@ def list_view(request):
     template = loader.get_template('blogging/list.html')
     context = {'posts': posts}
     body = template.render(context)
-    # return HttpResponse(body, content_type="text/html")
     return render(request, 'blogging\list.html', context)
+
+
+def detail_view(request, post_id):
+    published = Post.objects.exclude(published_date__exact=None)
+    try:
+        post = published.get(pk=post_id)
+    except Post.DoesNotExist:
+        raise Http404
+    context = {'post': post}
+    return render(request, 'blogging/detail.html', context)
