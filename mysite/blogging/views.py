ac1fa157d91807a4b39cb30f8927f48969fd1f29
Jared van der Duim <jared.vanderduim47@t-mobile.com>
2022-05-28 00:13:13 -0400

    shift blogging to class based views

--- mysite/blogging/views.py
+++ mysite/blogging/views.py
@@ -1,35 +1,30 @@
 from django.http import HttpResponse, HttpResponseRedirect, Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 
 from django.template import loader
 from blogging.models import Post
 
 
-def stub_view(request, *args, **kwargs):
-    body = "Stub View\n\n"
-    if args:
-        body += "Args:\n"
-        body += "\n".join(["\t%s" % a for a in args])
-    if kwargs:
-        body += "Kwargs:\n"
-        body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
-    return HttpResponse(body, content_type="text/plain")
+class BlogListView(ListView):
+    queryset = Post.objects.exclude(published_date=None).order_by('-published_date')
+    template_name = 'blogging/list.html'
 
 
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by('-published_date')
-    template = loader.get_template('blogging/list.html')
-    context = {'posts': posts}
-    body = template.render(context)
-    return render(request, 'blogging/list.html', context)
+# def detail_view(request, post_id):
+#     published = Post.objects.exclude(published_date__exact=None)
+#     try:
+#         post = published.get(pk=post_id)
+#     except Post.DoesNotExist:
+#         raise Http404
+#     context = {'post': post}
+#     return render(request, 'blogging/detail.html', context)
+class BlogDetailView(DetailView):
+    queryset = Post.objects.exclude(published_date__exact=None)
+    template_name = 'blogging/detail.html'
 
+    def post(self, request, *args, **kwargs):
+        post = get_object_or_404(self.queryset)
 
-def detail_view(request, post_id):
-    published = Post.objects.exclude(published_date__exact=None)
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404
-    context = {'post': post}
-    return render(request, 'blogging/detail.html', context)
+        context = {"object": post}
+        return render(request, self.template_name, context)
